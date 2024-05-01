cd82617ab22499e318db15fba50c78ef3369282f
aaronbolado <aaronjohncbolado@iskolarngbayan.pup.edu.ph>
2024-05-01 18:32:39 +0800

    Two way selection sort with flags

---
+++ MP2/ModifiedSelectionSort.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+// Declare prototypes
+void modifiedSelectionSort(int arr[], int n);
+void selectionSort(int arr[], int n);
+
+int main (void){
+    // Initialize array to sort 
+    const int arrSize = 10;
+    int testArray[arrSize] = {6, 2, 13, 62, 5, 2, 10, 8, 11, 41};
+
+    // Call standard selection sort
+    selectionSort(testArray, arrSize);
+
+    // Call modified selection sort 
+    modifiedSelectionSort(testArray, arrSize);
+ 
+    return 0;    
+}
+
+void modifiedSelectionSort(int arr[], int n) {
+    int i, j;       // Index for traversing from left     
+    int a, b;       // Index for traversing from right 
+    int cmpCount = 0;  // Tracks number of comparisons
+    bool flag;      // Tracks if a swap occurs in the iteration
+
+    // Traverse the array at both ends
+    for (i = 0, a = n-1; i < n || a >= 0; i++, a--) {       
+        int leftIndex = i, rightIndex = a;      // Index for the sorted portion of the array     
+        flag = false;                           // Set/Reset flag  
+
+        for (j = i+1, b = a-1; j < a || b > i; j++, b--) { // Traverses the array for values to compare 
+            if (arr[j] < arr[leftIndex]) {      // If current element is less in value          
+                leftIndex = j;                  
+                flag = true;                    // A swap occurs
+            }
+            if (arr[b] > arr[rightIndex]){      // If current element is higher in value
+                rightIndex = b;                 
+                flag = true;                    // A swap occurs
+            }
+            cmpCount++;
+        }
+
+        // Swaps element to its correct position
+        int tempLeft = arr[i];   
+        int tempRight = arr[a];   
+                       
+        arr[i] = arr[leftIndex];          
+        arr[a] = arr[rightIndex]; 
+
+        arr[leftIndex] = tempLeft;
+        arr[rightIndex] = tempRight;
+        
+        // Breaks loop if there were no swaps in previous iteration
+        if (flag != true) { 
+            printf("\nNo swaps occured. Array is already sorted");
+            break;  
+        }            
+    }
+
+    // Display output
+    printf("\nNumber of Comparisons: %d", cmpCount);
+    printf("\nModified Sorted Array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+void selectionSort(int arr[], int n) {
+    int i, j;                         
+    int cmpCount = 0;  
+
+    for (i = 0; i < n-1; i++) {             
+        int sortedIndex = i;                
+    
+        for (j = i+1; j < n; j++) {         
+            if (arr[j] < arr[sortedIndex])  
+                sortedIndex = j;     
+            cmpCount++;          
+        }
+    
+        int temp = arr[i];                  
+        arr[i] = arr[sortedIndex];          
+        arr[sortedIndex] = temp;            
+    }
+
+    // Display output
+    printf("\nNumber of Comparisons: %d", cmpCount);
+    printf("\nModified Sorted Array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
